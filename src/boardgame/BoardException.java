2700d38543d2459876bb4b8ac601f90f141d4acf
PetsuTHEPRO <78889539+PetsuTHEPRO@users.noreply.github.com>
2022-07-09 01:21:30 -0300

    Handling exceptions and clearing screen

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -2,13 +2,13 @@ package boardgame;
 
 public class BoardException extends RuntimeException{
 
-	/**
-	 * 
-	 */
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	
 	private static final long serialVersionUID = 1L;
 	
 	public BoardException(String msg) {
-		super(msg);
+		super(ANSI_RED + msg + ANSI_RESET);
 	}
 	
 
--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -4,13 +4,13 @@ import boardgame.BoardException;
 
 public class ChessException extends BoardException {
 
-	/**
-	 * 
-	 */
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
-		super(msg);
+		super(ANSI_RED + msg + ANSI_RESET);
 	}
 
 }
